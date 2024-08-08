e76b19b22530a508dc789864741222bcb26eb64a
siddhi <himanisahu739@gmail.com>
2024-08-08 19:21:01 +0530

    middleware created for gated routes

---
+++ backend/middleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+export function authMiddleWare(req, res, next) {
+  const authHeaders = req.headers.authorization;
+
+  if (!authHeaders || !authHeaders.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "no token detected. signin to continue",
+    });
+  }
+  const token = authHeaders.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.userId = decoded.userId;
+    req.username = decoded.username;
+    next();
+  } catch (error) {
+    console.log("error: " + error);
+    res.status(411).json({
+      message: "Invalid token",
+    });
+    return;
+  }
+}
