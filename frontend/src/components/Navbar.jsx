c5393325f671254ca3bed7acf0f7848daf595e16
siddhi <himanisahu739@gmail.com>
2024-08-14 11:14:39 +0530

    dashboard page first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between">
+      <div>Blog-it</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Dashboard = () => {
-  return <>hoo laa</>;
+  return (
+    <>
+      <div className="grid grid-cols-5 md:grid-cols-12 h-screen">
+        {/* sidebar */}
+        <div className=""></div>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
