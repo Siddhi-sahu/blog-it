fd6bb21bea461be9b64d8d1c91c2a7ae5dfb4181
siddhi <himanisahu739@gmail.com>
2024-08-14 15:01:04 +0530

    navbar added to dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
     <>
       <div className="grid grid-cols-5 md:grid-cols-12 h-screen">
         {/* sidebar */}
-        <div className="col-span-1 md:col-span-2 bg-gray-800 text-white p-4">
+        <div className="col-span-1 md:col-span-2 bg-pink-900 text-white p-4">
           <h2 className="text-lg md:text-xl font-semibold">Sidebar</h2>
           <ul className="mt-4">
             <li className="mb-2">Item 1</li>
