aad803bfb3dd773e7d254063cb956624ec0087e6
siddhi <himanisahu739@gmail.com>
2024-08-27 21:59:33 +0530

    improved styling of serach page

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -11,7 +11,7 @@ const Search = () => {
 
   useEffect(() => {
     const fetchResults = async () => {
-      if (query.length === 0) {
+      if (query.trim() === "") {
         setResults([]);
         return;
       }
@@ -56,34 +56,41 @@ const Search = () => {
       <Sidebar />
       <div className="col-span-4 md:col-span-10 bg-pink-100 p-6 overflow-y-auto">
         <Navbar />
-        <div className="flex justify-center mt-10">
-          <input
-            type="text"
-            placeholder="Search for blog posts..."
-            value={query}
-            onChange={handleInputChange}
-            className="w-full md:w-2/3 p-3 rounded-l-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-500"
-          />
-          <button className="bg-pink-500 hover:bg-pink-600 text-white font-semibold px-6 py-3 rounded-r-lg">
-            Search ideas
-          </button>
+        <div className="flex flex-col items-center mt-10">
+          <div className="w-full md:w-2/3 flex">
+            <input
+              type="text"
+              placeholder="Search for blog posts..."
+              value={query}
+              onChange={handleInputChange}
+              className="w-full md:w-2/3 p-3 rounded-l-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-500"
+            />
+            <button className="bg-pink-500 hover:bg-pink-600 text-white font-semibold px-6 py-3 rounded-r-lg">
+              Search ideas
+            </button>
+          </div>
 
           {loading && <div className="mt-8 text-center">loading...</div>}
           {error && <p className="text-red-600 text-center mt-4">{error}</p>}
 
-          <div className="mt-8">
+          <div className="mt-8 w-full md:w-2/3">
             {results.length > 0
               ? results.map((blog) => (
-                  <div key={blog._id}>
+                  <div
+                    key={blog._id}
+                    className="bg-white p-4 rounded-lg shadow mb-4"
+                  >
                     <h2>{blog.title}</h2>
-                    <p>
+                    <p className="text-gray-600">
                       By {blog.author.firstName} {blog.author.lastName}
                     </p>
-                    <p>{blog.blog}</p>
+                    <p className="text-gray-800">{blog.blog}</p>
                   </div>
                 ))
               : !loading && (
-                  <div className="block">No Matching Results{":("}</div>
+                  <div className="text-center text-gray-600">
+                    No Matching Results{":("}
+                  </div>
                 )}
           </div>
         </div>
